1e46b7173164b06fefe3cf6c86c15942439e7ff3
daipv6941-seta <daipv6941@seta-international.vn>
2023-12-25 12:26:55 +0700

    update

--- dockerPush.sh
+++ dockerPush.sh
@@ -1,3 +1,3 @@
 docker build -t daipv6941/dai-test:latest -f Personal/Dockerfile .
-docker login -u daipv6941 -p Daiphung2605<3@#$%2207
+docker login -u daipv6941 -p 'Daiphung2605<3@#$%2207'
 docker push daipv6941/dai-test:latest
